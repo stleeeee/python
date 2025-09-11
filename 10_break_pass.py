6379ef81aff303ee6b3ae5d4a6e036b06800c0f1
이시우 <siwoolee@email.com>
2025-09-11 11:19:45 +0900

    continue 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,16 @@
 cup = 0
 running = True
 
-while running:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        # running = False
-        break
+# while running:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         # running = False
+#         break
 
 print('while 문 종료')
+
+for i in range(1, 10):
+    if i % 3 == 0:
+        continue
+    print(i)
