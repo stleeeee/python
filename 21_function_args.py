5d82490e28eb341498b9e523ecdb3b52372c36f6
이시우 <siwoolee@email.com>
2025-09-15 10:00:24 +0900

    함수 인자값 (튜플로 받기)

--- 21_function_args.py
+++ 21_function_args.py
@@ -1,6 +1,17 @@
-def plus(num):
+# 인자값으로 아무것도 들어오지 않았을 경우 에러를 방지하기 위해 기본값 설정이 가능
+def plus(num = 0):
     result = num + 5
     return result
 
 print(plus(5)) # 10
 print(plus()) # plus() missing 1 required positional argument: 'num'
+
+# 인자값의 종류를 튜플(수정이 불가능환 List 형태)로만 받겠다.
+def tuple_args(*numbers):
+    print(numbers)
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+print(tuple_args(1, 2, 3, 4, 5))
