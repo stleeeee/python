e4326f23fd0244f4419ceef2f2d026e67ee5345a
이시우 <siwoolee@email.com>
2025-09-15 11:45:28 +0900

    모듈 사용 3

--- advance/01_module_use.py
+++ advance/01_module_use.py
@@ -1,7 +1,12 @@
 # from 모듈 import 함수
+# 사용할 함수를 미리 불러놓고 사용하는 방법
 from oper import sum
 print(f'sum() 함수 실행: {sum(5, 10)}')
 
 # import 모듈
+# 일단 모듈을 불러놓고 모듈로부터 원하는 함수를 사용하는 방법
 import oper as op
 print(f'minus() 함수 실행: {op.minus(10, 5)}')
+
+# 변수도 불러다 사용할 수 있다.
+print(f'field1: {op.field1} / field2: {op.field2}')
