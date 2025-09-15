19d81428e10647f0b78b537bba4afe7920cb9e1b
이시우 <siwoolee@email.com>
2025-09-15 10:16:07 +0900

    함수 인자값 (사전으로 받기 문제)

--- 21_function_args.py
+++ 21_function_args.py
@@ -14,4 +14,16 @@ def tuple_args(*numbers):
         total += num
     return total
 
+# 이 방식은 사용자가 인자값의 갯수를 자유롭게 정해서 넣을 수 있다.
 print(tuple_args(1, 2, 3, 4, 5))
+
+# ** 는 매개변수를 사전형태로 받겠다.
+def dic_args(**dic):
+    total = 0
+    for num in dic:
+        total += dic[num]
+    return total
+
+# 위 함수를 실행하면 입력된 값들의 합산이 반환되도록 하세요
+result = dic_args(kim = 50, lee = 100, park = 70, na = 90)
+print(result)
