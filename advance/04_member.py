0859b6b5ecd46a0c9c2d9fd697e68452d1ef5243
이시우 <siwoolee@email.com>
2025-09-15 14:16:25 +0900

    클래스 멤버

--- advance/04_member.py
+++ advance/04_member.py
@@ -1,8 +1,9 @@
 class Robot:
 
-    # 생성자?
+    # 생성자? 객체화 할 때 호출되는 함수의 일종으로
+    # 객체화가 될 때 가장 먼저 호출된다.
     def __init__(self):
-        pass
+        print('Robot 이 복사될 때 제일 먼저 호출되는 멤버')
 
     def doIt(self):
         print('나는 Robot 의 함수 입니다.')
