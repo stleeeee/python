5e3c3753583605eb428b39a6d23abb2487e03a36
이시우 <siwoolee@email.com>
2025-09-15 16:00:25 +0900

    멤버 호출

---
+++ advance/06_member_call.py
@@ -0,0 +1,16 @@
+class Car:
+    # 멤버 변수(필드)
+    gear = 0
+    on = False
+
+    # 생성자
+    def __init__(self, name):
+        pass
+
+
+    # 멤버 함수 - 클래스 안의 생성자 함수들은 해당 객체를 표시하기 위한 self 를 기본으로 가지고 있는다.
+    def start(self):
+        pass
+
+    def change(self):
+        pass
