c8f7a284cf9663ae9e91d8d199c91fc9a9b5cea3
이시우 <siwoolee@email.com>
2025-09-16 15:52:23 +0900

    다중 예외 처리 방식

--- advance/12_multi.py
+++ advance/12_multi.py
@@ -1,4 +1,5 @@
 # try 코드 안에서 여러개의 예외가 발생한 경우...
+import traceback
 
 # 1. 생겨난 예외마다 각기 다른 처릴르 해주고 싶을 때
 try:
@@ -13,5 +14,5 @@ except ValueError:
 # 2. 어떤 예외가 발생하던지 동일한 처리를 하고 싶을 때
 try:
     pass
-except Exception:
-    pass
+except Exception as e: # 예외의 최상위 부모이기 때문에 다른 자식 예외가 모두 다 들어옴. 그래서 디테일하게 무슨 예외인지는 모름.
+    traceback.print_exc()
