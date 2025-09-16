1726eedef88cdd92aca4a2dc97ca9b124ad5aba9
이시우 <siwoolee@email.com>
2025-09-16 12:54:34 +0900

    생성자 전달 순서 설명

--- advance/09_super_init.py
+++ advance/09_super_init.py
@@ -14,13 +14,18 @@ class SchoolMember:
     name = ""
     age = 0
 
-    def __init__(self, name, age):
+    def __init__(self, name, age): # 받아온 값으로 초기화 하고 객체화 된다.
         self.name = name
         self.age = age
 
-class Teacher(SchoolMember):
+class Teacher(SchoolMember): # 자식
     salary = 0
 
     def __init__(self, name, age, salary):
-        super().__init__(name, age)
-        self.salary = salary
+        super().__init__(name, age) # 2. 부모를 먼저 객체화 시키면서 초기화 할 값을 전달
+        self.salary = salary # 4. 그리고 나서 내가 초기화 하면서 객체화 된다.
+
+# 1. Teacher 라는 클래스를 객체화 한다. (초기화를 위해 매개변수를 전달)
+t = Teacher("김철수", 33, 50000000)
+# 5. name 과 age 는 부모 것 이지만 내 것처럼 내 객체에서 가져다 쓸 수 있게 된다.
+print(f'{t.name}({t.age}) - {t.salary}')
