54265853a50c28def8710f34f54aa27deba125a2
이시우 <siwoolee@email.com>
2025-09-16 14:51:01 +0900

    오버라이드 구현

--- advance/10_override.py
+++ advance/10_override.py
@@ -7,3 +7,13 @@ class Car:
 
     def stop(self):
         print('차가 멈춘다.')
+
+class MyCar(Car):
+
+    def run(self): # 부모와 같은 메서드를 사용하면 override 로 인식된다.
+        print('차가 시속 200km 로 달린다.')
+
+mc = MyCar()
+mc.start()
+mc.run()
+mc.stop()
