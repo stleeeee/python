0cace424bee5a9242e0be4761c44303b113f7820
이시우 <siwoolee@email.com>
2025-09-16 11:04:02 +0900

    정적 멤버

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -16,3 +16,16 @@ print(f'r2.count: {r2.count}')
 r2.count = 100
 print(f'r1.count: {r1.count}')
 print(f'r2.count: {r2.count}')
+
+# 원본의 내용을 고치고 싶다면? 원본으로 직접 가서 고쳐야 한다.
+Robot.count = 1000
+# 원본(static)영역에서 고쳤을 때 3당연히 복사본(heap) 영역에는 영향이 없다.
+
+print(f'r1.count: {r1.count}')
+print(f'r2.count: {r2.count}')
+r1.how_count()
+r2.how_count()
+
+# 마찬가지로 원본의 내용을 확인하고 싶다면 원본영역으로 가서 확인해야 한다.
+print(f'원본 count: {Robot.count}')
+print(f'원본 함수: {Robot.std_count()}')
