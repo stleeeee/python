dcbd88c7e546ee6aa2d1b172476e80994fc0eb0d
이시우 <siwoolee@email.com>
2025-09-16 10:38:49 +0900

    객체 사용

--- advance/06_member_call.py
+++ advance/06_member_call.py
@@ -3,14 +3,30 @@ class Car:
     gear = 0
     on = False
 
-    # 생성자
-    def __init__(self, name):
-        pass
-
+    # 생성자 - 클래스는 사용시 객체화를 하기 때문에 생성자는 필수다.
+    # 그런데 프로그래밍의 규칙 중 하나는 너무 당연하게 있어야 할것들은 생략이 가능하다.
+    def __init__(self):
+        # 혹시나 기어가 들어가 있거나 시동이 켜있을 수 있어 초기 상태로 되돌려 놓는다.
+        self.gear = 0
+        self.on = False
 
     # 멤버 함수 - 클래스 안의 생성자 함수들은 해당 객체를 표시하기 위한 self 를 기본으로 가지고 있는다.
     def start(self):
-        pass
+        if self.on == False:
+            print('시동이 걸렸습니다.')
+            self.on = True
+        else:
+            print('시동이 이미 걸려있습니다.')
 
-    def change(self):
-        pass
+    def change(self, gear):
+        print(f'{gear} 단으로 변속 했습니다.')
+        self.gear += gear
+
+# Car 클래스를 객체화 (복사)
+# 객체를 통해 사용하고 싶은 멤버 호출
+car = Car()
+# 시동 걸기
+car.start()
+# 변속하기
+car.change(3)
+print(f'현재 car 의 gear 단수: {car.gear}')
