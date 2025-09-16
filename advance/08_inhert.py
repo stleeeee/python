de9245a640bff891143511d06e9d13bf8e086aa0
이시우 <siwoolee@email.com>
2025-09-16 12:04:38 +0900

    jumper 만들기

---
+++ advance/08_inhert.py
@@ -0,0 +1,16 @@
+class Runner:
+    def run(self):
+        pass
+
+    def sprint(self):
+        pass
+
+class Jumper:
+    def jump(self):
+        pass
+
+    def high_jump(self):
+        pass
+
+class Person(Jumper, Runner):
+    pass
