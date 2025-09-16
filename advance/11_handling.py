e59aab41cc68337433710c2b515cde0e7910a562
이시우 <siwoolee@email.com>
2025-09-16 15:43:30 +0900

    예외 처리

--- advance/11_handling.py
+++ advance/11_handling.py
@@ -1,3 +1,5 @@
+import traceback
+
 num_list = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 4, 5, 6]
 
 idx = 0
@@ -7,7 +9,9 @@ try:
         idx = num_list.index(3, idx) # ValueError: 3 is not in list
         print(f'3은 {idx} 인덱스에 있습니다.')
         idx += 1
-except ValueError:
+except ValueError as e:
+    print(e) # 예외에 대한 대력적인 정보 출력
+    traceback.print_exc() # 상세한 예외 정보(개발자에게 필요한 정보)
     print('더 이상 3을 찾을 수 없습니다.')
 finally:
     print('====끝====')
