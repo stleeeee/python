a89917a1cc2462cd216636e7e2d53da4e590f89e
이시우 <siwoolee@email.com>
2025-09-16 14:55:33 +0900

    오버라이드 구현2

--- advance/10_override.py
+++ advance/10_override.py
@@ -10,10 +10,17 @@ class Car:
 
 class MyCar(Car):
 
+    turbo = False
+
     def run(self): # 부모와 같은 메서드를 사용하면 override 로 인식된다.
-        print('차가 시속 200km 로 달린다.')
+        if self.turbo == True:
+            print('차가 시속 200km 로 달린다.')
+        else:
+            super().run() # 부모의 run 을 그대로 쓰겠다.
 
 mc = MyCar()
 mc.start()
+mc.run()
+mc.turbo = True
 mc.run()
 mc.stop()
