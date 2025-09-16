212a002a3987662716481c1ace495530b82f258d
이시우 <siwoolee@email.com>
2025-09-16 15:47:22 +0900

    다중 예외 처리 방식

---
+++ advance/12_multi.py
@@ -0,0 +1,18 @@
+# try 코드 안에서 여러개의 예외가 발생한 경우...
+from importlib.metadata import pass_none
+
+# 1. 생겨난 예외마다 각기 다른 처릴르 해주고 싶을 때
+try:
+    pass
+except IOError:
+    pass
+except KeyboardInterrupt:
+    pass
+except ValueError:
+    pass
+
+# 2. 어떤 예외가 발생하던지 동일한 처리를 하고 싶을 때
+try:
+    pass
+except Exception:
+    pass
